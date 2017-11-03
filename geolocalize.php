d3372c8757d4f0046118f5e71f3c44075a5cc4a1
jief <jean-francois@monfort.me>
2017-11-03 09:25:15 +0000

    Apply fixes from StyleCI

--- geolocalize.php
+++ geolocalize.php
@@ -1,54 +1,53 @@
 <?php
+
 require 'vendor/autoload.php';
 
-
 /**
- * Use Openstreetmap API to extract lng & lat for each belgian city
+ * Use Openstreetmap API to extract lng & lat for each belgian city.
  *
  * @author Jean-Francois Monfort <jf.monfort@gmail.com>
  */
+$file = 'zipcodes_num_fr.csv';
+$new_file = 'zipcode-belgium.csv';
+$new_file_json = 'zipcode-belgium.json';
+$export = [];
 
-$file = "zipcodes_num_fr.csv";
-$new_file = "zipcode-belgium.csv";
-$new_file_json = "zipcode-belgium.json";
-$export = array();
+if (($handle = fopen($file, 'r')) !== false) {
+    $fp = fopen($new_file, 'w');
 
-if (($handle = fopen($file, "r")) !== FALSE) {
-  $fp = fopen($new_file, 'w');
-
-  $geocoder = new \Geocoder\Geocoder();
-  $adapter = new \Geocoder\HttpAdapter\CurlHttpAdapter();
-  $chain = new \Geocoder\Provider\ChainProvider(array(
+    $geocoder = new \Geocoder\Geocoder();
+    $adapter = new \Geocoder\HttpAdapter\CurlHttpAdapter();
+    $chain = new \Geocoder\Provider\ChainProvider([
     new \Geocoder\Provider\OpenStreetMapProvider($adapter, 'be'),
-  ));
-  $geocoder->registerProvider($chain);
+  ]);
+    $geocoder->registerProvider($chain);
 
-  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-    if($data[2] == "") {
-      continue;
+    while (($data = fgetcsv($handle, 1000, ',')) !== false) {
+        if ($data[2] == '') {
+            continue;
+        }
+
+        try {
+            $addr = $data[0].' '.$data[1].' Belgium';
+            $geocode = $geocoder->geocode($addr);
+
+            $row = [
+        'zip'  => $data[0],
+        'city' => mb_convert_case($data[1], MB_CASE_TITLE, 'UTF-8'),
+        'lng'  => $geocode->getLongitude(),
+        'lat'  => $geocode->getLatitude(),
+      ];
+            $export[] = $row;
+            fputcsv($fp, $row);
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+        sleep(1);
     }
+    fclose($fp);
+    fclose($handle);
 
-    try {
-      $addr = $data[0] . ' ' . $data[1]. ' Belgium';
-      $geocode = $geocoder->geocode($addr);
-
-      $row = array(
-        "zip" => $data[0],
-        "city" => mb_convert_case($data[1], MB_CASE_TITLE, "UTF-8"),
-        "lng" => $geocode->getLongitude(),
-        "lat" => $geocode->getLatitude(),
-      );
-      $export[] = $row;
-      fputcsv($fp, $row);
-    } catch (\Exception $e) {
-        echo $e->getMessage();
-    }
-    sleep(1);
-  }
-  fclose($fp);
-  fclose($handle);
-
-  $fp = fopen($new_file_json, 'w');
-  fwrite($fp, json_encode($export));
-  fclose($fp);
+    $fp = fopen($new_file_json, 'w');
+    fwrite($fp, json_encode($export));
+    fclose($fp);
 }
